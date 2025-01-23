451dd490c3d287ffbdc5a809833d8a3fafbde396
Jessica McInchak <jessica.mcinchak@gmail.com>
2025-01-23 13:04:51 +0100

    update active sessions

---
+++ jan2025_revisedSessions/client.js
@@ -0,0 +1,87 @@
+require("isomorphic-fetch");
+
+class Client {
+  constructor({ hasuraSecret, targetURL }) {
+    this.secret = hasuraSecret;
+    this.url = targetURL;
+  }
+
+  async graphQL(query, variables) {
+    const result = await fetch(this.url, {
+      method: "POST",
+      body: JSON.stringify({ query, variables }),
+      headers: {
+        "x-hasura-admin-secret": this.secret,
+      },
+    });
+    return result.json();
+  }
+
+  async getQueuedFlow() {
+    const { data, errors } = await this.graphQL(
+      `query GetQueuedFlow {
+        temp_data_migrations_audit(
+          limit: 1, 
+          where: {updated: {_eq: false}}
+        ) {
+          id: flow_id
+          flow {
+            slug
+            team {
+              slug
+            }
+            sessions: lowcal_sessions(
+              where: {
+                created_at: {_gte: "2024-12-22"},
+                submitted_at: {_is_null: true}, 
+              }
+            ) {
+              id
+              data
+            }
+          }
+        }
+      }`
+    );
+
+    if (errors || !data) throw new Error(formatJSON({ data, errors }));
+    return data.temp_data_migrations_audit[0];
+  }
+
+  async updateSessionData(sessionId, sessionData) {
+    const { data, errors } = await this.graphQL(
+      `mutation UpdateLowcalSession ($id: uuid!, $data: jsonb!) {
+        update_lowcal_sessions_by_pk(pk_columns: {id: $id}, _set: {data: $data}) {
+          id
+        }
+      }`,
+      {
+        id: sessionId,
+        data: sessionData,
+      }
+    );
+    if (errors || !data) throw new Error(formatJSON({ data, errors }));
+    return { ...data };
+  }
+
+  async updateAuditTable(flowId) {
+    const { data, errors } = await this.graphQL(
+      `mutation UpdateFlow ($flowId: uuid!) {
+        update_temp_data_migrations_audit_by_pk(pk_columns: {flow_id: $flowId}, _set: {updated: true}) {
+          flow_id
+        }
+      }`,
+      {
+        flowId: flowId,
+      }
+    );
+    if (errors || !data) throw new Error(formatJSON({ data, errors }));
+    return { ...data };
+  }
+}
+
+function formatJSON({ data, errors }) {
+  return JSON.stringify({ data, errors }, null, 2);
+}
+
+module.exports = Client;
---
+++ jan2025_revisedSessions/index.js
@@ -0,0 +1,70 @@
+const chalk = require("chalk");
+const Client = require("./client");
+const { migrateSessionData } = require("./helpers");
+
+const timestamp = `[${new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')}]`;
+
+(async function go() {
+  const url = {
+    production: "https://hasura.editor.planx.uk/v1/graphql",
+    staging: "https://hasura.editor.planx.dev/v1/graphql",
+    local: "http://localhost:7100/v1/graphql",
+  };
+
+  const hasuraSecret = process.env["HASURA_SECRET"];
+  const env = process.env["HASURA_ENV"];
+  console.log(chalk.cyan(`${timestamp} Connecting to Hasura ${env}`));
+
+  // Create GraphQL client
+  const client = new Client({
+    hasuraSecret,
+    targetURL: url[env],
+  });
+
+  // Get a queued flow that hasn't been updated yet
+  const { id, flow } = await client.getQueuedFlow();
+  const flowSlug = `${flow?.team?.slug}/${flow?.slug}`;
+
+  if (!id) return;
+  
+  if (id) {
+    try {
+      const isPublished = flow.publishedFlows?.length > 0;
+      const isPublishedWithSessions = isPublished && flow.sessions?.length > 0;
+
+      console.log(`${timestamp} Fetching flow ${flowSlug}`);
+
+      if (isPublishedWithSessions) {
+        // Iterate through sessions and update them individually if they have FindProperty or PlanningConstraints breadcrumb
+        for (const session of flow.sessions) {
+          const findPropertyNodeId = Object.entries(flow.data).find(([_nodeId, nodeData]) => nodeData.type === 9)?.[0];
+          const hasFindPropertyBreadcrumb = Object.keys(session.data?.breadcrumbs).includes(findPropertyNodeId);
+          const planningConstraintsNodeId = Object.entries(flow.data).find(([_nodeId, nodeData]) => nodeData.type === 11)?.[0];
+          const hasPlanningConstraintsBreadcrumb = Object.keys(session.data?.breadcrumbs).includes(planningConstraintsNodeId);
+
+          if (hasFindPropertyBreadcrumb || hasPlanningConstraintsBreadcrumb) {
+            console.log(`${timestamp} Updating session ${session.id} (${flowSlug})`);
+            const sessionData = migrateSessionData(session.data);
+
+            // Write session data to database
+            const sessionDataResponse = await client.updateSessionData(session.id, sessionData);
+            if (
+              sessionDataResponse?.update_lowcal_sessions_by_pk?.id
+            ) {
+              console.log(`${timestamp} Successfully updated session ${session.id} (${flowSlug})`);
+            }
+          } else {
+            console.log(`${timestamp} Skipping session without FindProperty or PlanningConstraints breadcrumb ${session.id} (${flowSlug})`);
+          }
+        };
+      } else {
+        console.log(`${timestamp} Skipping flow without active sessions ${flowSlug}`);
+      }
+
+      // Mark the audit table as "updated" once we've checked a flow regardless of finding sessions
+      const _auditTableResponse = await client.updateAuditTable(id);
+    } catch (error) {
+      console.log(chalk.red(`Error": ${error}`));
+    }
+  }
+})();
